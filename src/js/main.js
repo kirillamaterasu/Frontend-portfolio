068e4b50a2517fea0e53897a90d8e77c5af80f44
kirillamaterasu <80434189+kirillamaterasu@users.noreply.github.com>
2021-07-20 15:21:11 +0500

    main commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
+$(document).ready(function() {
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+});
+
+$('.menu__btn').on('click', function () {
+  $('.menu__btn-line').toggleClass('change');
+});
+
+
+  $('.project__slider').slick({
+    dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="../img/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="../img/arrow-right.svg" alt=""></button>',
+  });
+
+});
